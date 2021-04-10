efe7291701c7b347cdad3edab891c531382b595f
stephanielambrighs <stephanielambrighs@hotmail.com>
2021-04-10 22:59:32 +0200

    feat: added url paths for routes

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('votes/index', {});
+});
+
+router.get('/updatevotes', function(req, res, next){
+  res.render('votes/updatevotes', {});
 });
 
 module.exports = router;
