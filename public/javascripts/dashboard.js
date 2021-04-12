a3ea8d7734e6c50ba559ed5bea1bdac16f4b9f6b
stephanielambrighs <stephanielambrighs@hotmail.com>
2021-04-12 14:23:31 +0200

    feat: added dashboard.js

---
+++ public/javascripts/dashboard.js
@@ -0,0 +1,21 @@
+primus = Primus.connect("http://localhost:3000", {
+    reconnect: {
+        max: Infinity // Number: The max delay before we try to reconnect.
+      , min: 500 // Number: The minimum delay before we try reconnect.
+      , retries: 10 // Number: How many times we should try to reconnect.
+    }
+});
+
+document.querySelector("div").addEventListener("click", function(e){
+  // let docent = document.querySelector(".docent");
+  let docent = e.target.parentElement.getAttribute("class");
+  console.log(docent);
+  
+  primus.write({
+    name: docent
+  });
+
+  e.preventDefault();
+});
+
+
