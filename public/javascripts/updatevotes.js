3952018b2a52b78af564380154f330a390a1e5ce
stephanielambrighs <stephanielambrighs@hotmail.com>
2021-04-12 14:23:46 +0200

    feat: added updatevotes.js

---
+++ public/javascripts/updatevotes.js
@@ -0,0 +1,25 @@
+// const e = require("express");
+
+primus = Primus.connect("http://localhost:3000", {
+    reconnect: {
+        max: Infinity // Number: The max delay before we try to reconnect.
+      , min: 500 // Number: The minimum delay before we try reconnect.
+      , retries: 10 // Number: How many times we should try to reconnect.
+    }
+});
+
+
+primus.on('data', (data) => {
+    
+    let docent = data.name;
+    console.log(docent);
+    
+    let score = Number(document.querySelector(`.${docent} .score span`).innerHTML);
+    
+    console.log(typeof score);
+    console.log(score + 1);
+
+    document.querySelector(`.${docent} .score span`).innerHTML = score += 1;
+
+    
+});
